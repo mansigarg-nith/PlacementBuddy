4b9599df06fa2ddef86920d55f4e74e67a933ebf
Mansi <mansigarg2280@gmail.com>
2022-09-30 18:24:13 +0530

    Db error resolved

--- company/models.py
+++ company/models.py
@@ -1,10 +1,10 @@
+from email.policy import default
 from django.db import models
-from drive.models import Drive
 # Create your models here.
 class Company(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
     # 0 - IT, 1 - Core
-    type = models.BooleanField()
+    type = models.BooleanField(default=0)
 
     def __str__(self):
         return self.name
--- drive/models.py
+++ drive/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from student.models import Branch
+from company.models import Company
 # Create your models here.
+
+class Year(models.Model):
+    year = models.IntegerField()
+
+    def __str__(self):
+        return str(self.year)
+
 class Drive(models.Model):
     company = models.ForeignKey('company.Company', on_delete=models.CASCADE)
     profile = models.CharField(max_length=100)
@@ -9,18 +17,13 @@ class Drive(models.Model):
     job_desc = models.TextField(max_length=2000)
     from_date = models.DateField()
     to_date = models.DateField()
-    year = models.ForeignKey('Year', on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
     selected = models.IntegerField()
     def __str__(self):
         return self.profile
 
 class AllowedBranch(models.Model):
-    drive = models.ForeignKey('Drive', on_delete=models.CASCADE)
+    drive = models.ForeignKey(Drive, on_delete=models.CASCADE)
     branch = models.ForeignKey('student.Branch', on_delete=models.CASCADE)
     
 
-class Year(models.Model):
-    year = models.IntegerField()
-
-    def __str__(self):
-        return str(self.year)
