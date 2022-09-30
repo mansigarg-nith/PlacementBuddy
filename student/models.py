12efc50849fb99315d7455f8c70fbcf32d169688
Mansi <mansigarg2280@gmail.com>
2022-09-30 13:10:06 +0530

    App student

---
+++ student/models.py
@@ -0,0 +1,17 @@
+from sre_constants import BRANCH
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=50)
+    mname = models.CharField(max_length=50, blank=True, null=True)
+    lname = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    roll = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100)
+    batch = models.CharField(max_length=100)
